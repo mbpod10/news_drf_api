6e672fd60f011043b365aa3ffad6c0e939a2788c
M. Brock Podgurski <mbpod10@gmail.com>
2022-02-19 13:02:52 -0500

    created CRUD class-based views

---
+++ app/news/class_views.py
@@ -0,0 +1,49 @@
+from .models import Article
+from .serializers import ArticleSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
+
+
+class ArticleList(APIView):
+
+    def get(self, request, format=None):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ArticleDetailView(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Article.objects.get(pk=pk)
+        except Article.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        article = self.get_object(pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk, format=None):
+        article = self.get_object(pk=pk)
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_200_OK)
+
+    def delete(self, request, pk, format=None):
+        article = self.get_object(pk=pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- app/news/urls.py
+++ app/news/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import article_list_view, article_detail_view
+from .function_views import article_list_view, article_detail_view
+from .class_views import ArticleList, ArticleDetailView
 urlpatterns = [
     path('articles/', article_list_view),
     path('articles/<int:pk>', article_detail_view),
+    path('articles_/', ArticleList.as_view()),
+    path('articles_/<int:pk>', ArticleDetailView.as_view()),
 ]
